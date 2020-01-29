351794ae9d9b6a929a7845f33a96def8b517f521
Gagan jai <55681648+gaganjain280@users.noreply.github.com>
2020-01-29 16:54:29 +0530

    Update design_factory.php
    make it done using two interface

--- design_factory.php
+++ design_factory.php
@@ -6,59 +6,53 @@
  * Author: Gagan
  * Author URI: http://gagan.codingkloud.com
  * Version: 1.0.1
-   */
- 
+ */
 class FactoryDesign{
 public function __construct(){     
-    //****************** enque Script file *******************//
-           add_action( 'wp_enqueue_scripts', array($this,'loadJs' ));     
-   
-    //****************** Load main method  *******************//
-           add_action('wp_ajax_factroyData', array($this,'factroyData'));
-           add_action('wp_ajax_nopriv_factroyData', array($this,'factroyData'));
- } 
- public  function loadJs() {       
-    wp_enqueue_script( 'my_loadmore',  plugins_url(). '/design_pattern/designPattern.js', array( 'jquery' ));
-     wp_localize_script( 'my_loadmore', 'ajax_object', array( 'ajaxurl' =>admin_url( 'admin-ajax.php' ) ) );
-         wp_enqueue_script( 'factory_script');
-         // wp_localize_script( 'factory_script', 'ajax_admin', array( 'ajax_url' => admin_url( 'admin-ajax.php' ) ) );
- }
+        //***************** enque Script file *******************//
+        add_action( 'wp_enqueue_scripts', array($this,'add_factory_scripts' ));  
+        //****************** Load main method  *******************//                
+        add_action('wp_ajax_factroyData', array($this,'factroyData'));
+        add_action('wp_ajax_nopriv_factroyData', array($this,'factroyData'));
 
-     //****************** Main responsible for get data from front end *******************//              
- public function factroyData(){
-        $html="";
-        $html.="<table>";       
-        $oderDetails = new FactoryMain;
+ } 
+public  function add_factory_scripts() {       
+        
+        wp_enqueue_script( 'my_loadmore',  plugins_url(). '/design_pattern/designPattern.js', array( 'jquery' ));
+        wp_localize_script( 'my_loadmore', 'ajax_object', array( 'ajaxurl' =>admin_url( 'admin-ajax.php' ) ) );
+        wp_enqueue_script( 'factory_script');
+        }
+                   
+     //********* Main responsible for get data from front end **************//        
+public function factroyData(){
+        $thtml          ="";
+        $thtml.="<table>";       
+        $oderDetails   = new FactoryMain;
         $BaseTransport = new BaseTransport;
-        $factory_type=$_POST["factory_type"];            
-        $quantity =$_POST["quantity"];
+        $factory_type  =$_POST["factory_type"];            
+        $quantity      =$_POST["quantity"];
         $oderDetails->materialquantity = $quantity;
         if ($factory_type=="2") 
         {       
-        $oderDetails->cost = "12000";
-        $oderDetails->totaltime = "2 Day";
-// function create with perameter is used for helping subclass for object creation  
-        $order = $BaseTransport->create("Sea", $oderDetails);
-        $html.="<tr><th>Transport by Sea</th></tr>";
+         $oderDetails->cost      = "12000";
+         $oderDetails->totaltime = "2 Day";
+         $order                  = $BaseTransport->createTransport("Sea", $oderDetails);
+         $thtml.="<tr><th>Transport by Sea</th></tr>";
         }
          else{
-        $oderDetails->totaltime = "1 Day";  
-        $oderDetails->cost = "10000";
-        $order = $BaseTransport->create("Road", $oderDetails);
-        $html.="<tr><th>Transport by Road</th></tr>";
-        ?>
-        <?php
-         }
-       $html.="<tr><td>" . $order->cost()."</td></tr>"; 
-       $html.="<tr><td>" . $order->totaltime()."</td></tr>"; 
-       $html.="<tr><td>" . $order->materialquantity()."</td></tr>"; 
-       echo $html;   
+         $oderDetails->totaltime = "1 Day";  
+         $oderDetails->cost      = "10000";
+         $order                  = $BaseTransport->createTransport("Road", $oderDetails);
+         $thtml.="<tr><th>Transport by Road</th></tr>";
+        }
+        $thtml.="<tr><td>" . $order->cost()."</td></tr>"; 
+        $thtml.="<tr><td>" . $order->totaltime()."</td></tr>"; 
+        $thtml.="<tr><td>" . $order->materialquantity()."</td></tr>"; 
+       echo $thtml;   
       wp_die();  
     }
-
 }
 new FactoryDesign();
-
 
 interface Transport{
      public function cost();
@@ -73,42 +67,44 @@ class FactoryMain{
 }
 // here we can implement as many subclasses we want, just need to implement interface
 class Road implements Transport{
-    private $make;
+    private $storeOderDetail;
     public function __construct(FactoryMain $oderDetails) {
-        $this->make = $oderDetails;
+        $this->storeOderDetail = $oderDetails;
     }
     public function cost(){
-        return "Total Cost (".$this->make->cost .")<br/>";
+        return "Total Cost ".$this->storeOderDetail->cost ."<br/>";
     }
     
     public function totaltime(){
-        return " Delivery Time (".$this->make->totaltime .")<br/>";
+        return " Delivery Time ".$this->storeOderDetail->totaltime ."<br/>";
     }
     public function materialquantity(){
-        return "Material Quantity (".$this->make->materialquantity .")<br/>";
+        return "Material Quantity ".$this->storeOderDetail->materialquantity ."<br/>";
     }
 }
 class Sea implements Transport{
-    private $make;
+    private $storeOderDetail;
     public function __construct(FactoryMain $oderDetails) {
-        $this->make = $oderDetails;
+        $this->storeOderDetail = $oderDetails;
     }
     public function cost(){
-        return "Total Cost (".$this->make->cost .")<br/>";
+        return "Total Cost ".$this->storeOderDetail->cost ."<br/>";
     }
     public function totaltime(){
-        return " Delivery Time (".$this->make->totaltime .")<br/>";
+        return " Delivery Time ".$this->storeOderDetail->totaltime ."<br/>";
     }
     public function materialquantity(){
-        return "Material Quantity (".$this->make->materialquantity .")<br/>";
+        return "Material Quantity ".$this->storeOderDetail->materialquantity ."<br/>";
     }
 }
-
-// base class used for decision making,  that which class object should created
-class BaseTransport{
-    public function create($class, $make)
+    interface LogisticsFactory {
+    public function createTransport($class, $storeOderDetail);
+    }
+    // BaseTransport class used for decision making, that which class object should created
+    class BaseTransport implements LogisticsFactory {
+    public function createTransport($class, $storeOderDetail)
     {
-        return new $class($make);
+        return new $class($storeOderDetail);
     }
 }
 
